2abb1e8aed9361099643794cba8081a9a78af691
Rosa Ejlskov Jensen <Rosa.jensen@rocketmail.com>
2017-02-06 13:51:40 +0100

    CODE

--- index1.js
+++ index1.js
@@ -1,5 +1,5 @@
 //Adressen på Bluetooth Mate, Password er 1234
-var macAddress = "00:06:66:7B:AD:2F";
+var macAddress = "00:06:66:7D:83:8D";
 
 function onLoad(){
 	document.addEventListener("deviceready", onDeviceReady, false);
